d059e0d5dbefb59f757d328fdc2b4eb21800fe44
Luna <ojackson287@gmail.com>
2022-07-24 18:45:55 -0400

    feat: so much crap

---
+++ src/exts/bumping.py
@@ -0,0 +1,108 @@
+from nextcord.ext import tasks, commands
+import nextcord
+from loguru import logger
+import re
+import datetime
+
+
+class Minutes(commands.Converter):
+    async def convert(self, ctx: commands.Context, argument: str) -> float:
+        minutes: float = 0.0
+
+        time_legend = {
+            "s" : 1/60,
+            "m" : 1,
+            "h" : 60,
+            "d" : 1440,
+            "w" : 10080
+        }
+
+        pattern = re.compile("\d+[smhdw]")
+        for match in re.finditer(pattern, argument):
+            s = match[0]
+            minutes += time_legend[s[-1]] * int(s[0:-1])
+
+        return minutes
+
+
+class Bumping(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self._scheduled_threads: dict[int, tuple(tasks.Loop, datetime.datetime)] = {}
+    
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        logger.info("Bumping cog has been fully loaded!")
+
+
+    @commands.group(invoke_without_command=True)
+    async def bump(self, ctx: commands.Context, thread: nextcord.Thread, message: str = "Bump!", *, silent: bool = False):
+        """Sends a message to an arbitrary discord thread."""
+
+        await thread.send(message)
+        logger.info(f"Bumped {thread.name} (id: {thread.id})")
+
+        if not silent:
+            await ctx.send(f"Thread {thread.name} bumped!")
+    
+
+    @bump.command()
+    async def schedule(self, ctx: commands.Context, thread: nextcord.Thread, interval: Minutes, message: str = "Bump!"):
+        """Sends a message to an arbitrary discord thread in a regular interval."""
+
+        if interval > thread.auto_archive_duration:
+            await ctx.send("The interval time is larger than the auto archive time. Provide a shorter interval.")
+            return
+
+        @tasks.loop(minutes=interval)
+        async def task():
+            await self.bump(ctx, thread, message, silent=True)
+
+        self._scheduled_threads[thread.id] = (task, datetime.datetime.now(tz=datetime.timezone.utc))
+
+        task.start()
+        await ctx.send(f"Thread {thread.name} will be bumped every {interval} minutes.")
+        logger.info(f"Started new bump task on thread {thread.name} (id: {thread.id})")
+
+
+    @bump.command()
+    async def unschedule(self, ctx: commands.Context, thread: nextcord.Thread):
+        """Removes a thread from being bumped further."""
+
+        task_object: tasks.Loop = self._scheduled_threads.get(thread.id, None)
+
+        if task_object is None:
+            await ctx.send(f"Thread {thread.name} is not scheduled for bumping.")
+            return
+        
+        task_object.cancel()
+        logger.info(f"Removed task for thread {thread.name} (id: {thread.id})")
+    
+
+    @bump.command()
+    async def status(self, ctx: commands.Context, thread: nextcord.Thread):
+        """Displays the scheduling status of a thread."""
+        
+        embed: nextcord.Embed = nextcord.Embed()
+        embed.title = f"Status of thread {thread.name}"
+
+        if thread.id not in self._scheduled_threads:
+            embed.color = nextcord.Color.red()
+            embed.add_field(name="Scheduled", value="false", inline=False)
+            embed.add_field(name="Next bump", value="N/A", inline=False)
+            embed.add_field(name="Bump task created at", value="N/A", inline=False)
+        
+        else:
+            format_str = r"%b %d %Y %H:%M:%S %Z"
+            utc_time: datetime.datetime = self._scheduled_threads[thread.id][0].next_iteration.astimezone(tz=datetime.timezone.utc)
+            next_bump: str = utc_time.strftime(format_str)
+            creation_date = self._scheduled_threads[thread.id][1].strftime(format_str)
+
+            embed.color = nextcord.Color.green()
+            embed.add_field(name="Scheduled", value="true", inline=False)
+            embed.add_field(name="Next bump", value=next_bump, inline=False)
+            embed.add_field(name="Bump task created at", value=creation_date, inline=False)
+        
+        await ctx.send(embed=embed)
+
--- src/main.py
+++ src/main.py
@@ -1,17 +1,75 @@
+from tkinter import PAGES
+from discord import Color
+from dotenv import dotenv_values
+from loguru import logger
 import nextcord
 from nextcord.ext import commands
-from dotenv import dotenv_values
+from exts.bumping import Bumping
+from exts.threads import Threads
 
-TESTING_GUILD_ID = 870126657766301697  # Replace with your guild ID
 
 bot = commands.Bot()
+bot.command_prefix = "%"
+
+bot.add_cog(Bumping(bot))
+bot.add_cog(Threads(bot))
+
+
+class HelpView(nextcord.ui.View):
+    """Represents the Help embed."""
+
+    def __init__(self):
+        self._page_number = 1
+        
+        # build pages
+        self.cog_pages: dict[int, tuple[str, dict]] = {k: (v[0], self._create_page(v[1])) for k, v in enumerate(bot.cogs, start=1)}
+        logger.debug(f"built pages: {self.cog_pages}")
+    
+    def _create_page(self, cog: commands.Cog) -> dict:
+        """Builds an embed detailing commands out of a cog."""
+        
+        embed_object = {
+            "title" : f"Cog {cog.qualified_name}",
+            "type" : "rich",
+            "description" : "Registered commands",
+            "color" : nextcord.Color.yellow(),
+            "fields" : []
+        }
+
+        for c in cog.walk_commands():
+            logger.debug(f"command/group found: {c.qualified_name}")
+
+        return embed_object
+
+    def get_embed_object(self, page: int):
+        try:
+            return self.cog_pages[page][1]
+        except KeyError:
+            return None
+        
+    @nextcord.ui.button(label="Back", style=nextcord.ButtonStyle.blurple)
+    async def back(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+        pass
+
+    @nextcord.ui.button(label="Forward", style=nextcord.ButtonStyle.blurple)
+    async def forward(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+        pass
+
+
+TESTING_GUILD_ID = 840691233185988628  # Replace with your guild ID
+
 
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    logger.info(f"Logged in as {bot.user}")
 
-@bot.slash_command(description="My first slash command", guild_ids=[TESTING_GUILD_ID])
-async def hello(interaction: nextcord.Interaction):
-    await interaction.send("Hello!")
+@bot.slash_command(guild_ids=[TESTING_GUILD_ID])
+async def help(interaction: nextcord.Interaction):
+    obj = HelpView()
+    await interaction.send("test embed", view=obj, embed=obj.get_embed_object(1))
+
+@bot.slash_command(guild_ids=[TESTING_GUILD_ID])
+async def reload(interaction: nextcord.Interaction):
+    pass
 
 bot.run(dotenv_values(".env")["TOKEN"])
